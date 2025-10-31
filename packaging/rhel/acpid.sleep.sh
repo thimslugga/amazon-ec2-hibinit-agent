f9cef063cacb8582588d6d3c43feef4c22906fe5
Adam Kaminski <adam@adamkaminski.com>
2025-10-31 11:31:23 -0400

    Refactor acpid sleep.sh
    Minor improvements

--- packaging/rhel/acpid.sleep.sh
+++ packaging/rhel/acpid.sleep.sh
@@ -8,21 +8,21 @@ failed='false'
 # target by setting to maximum priority.
 swap_priority=32767
 
-hibernate()
-{
+hibernate() {
+        # Enable swap file and then hibernate
         swapon --priority=$swap_priority /swap && systemctl hibernate
         if [ $? -ne 0 ]; then
-            logger "Hibernation failed, Sleeping 2 mins before retry"
+            logger "Hibernation failed. Sleep for 2-min before retry attempt."
             failed='true'
             swapoff /swap
-            sleep 2m
+            sleep 120
         else
             failed='false'
         fi
 }
 
 case "$2" in
-    SBTN)
+    SLPB|SBTN)
         # The iteration had been placed here to add retry logic to hibernation 
         # in case of failures and to avoid force stop of instances after 20min
         for i in 1 2 3; do
